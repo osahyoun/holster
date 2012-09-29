d88e2ffdb45c4ab96db18748efe0295f1d423b2b
Omar Sahyoun <osahyoun@gmail.com>
2012-09-29 15:19:19 +0100

    Parsing text for github issue requests.

--- plugins/issues.rb
+++ plugins/issues.rb
@@ -1,5 +1,48 @@
 module Plugins
-  # For creating and updating GitHub Issues  
+  # For creating and updating GitHub Issues
   class Issues
+    class Parser
+
+      def initialize(text)
+        @text = text
+      end
+
+      def tokens
+        @tokens ||= @text.split('|').map(&:strip)
+      end
+
+      def attr(name)
+        [tokens.find{ |t| t =~ /^#{name}:\s*(.+)$/ }, $1].last
+      end
+
+      def token(name)
+        parse(name).first
+      end
+
+      def milestone
+        # TODO
+      end
+
+      def title
+        tokens.first
+      end
+
+      def body
+        tokens[1]
+      end
+
+      def assignee
+        attr(/assigne{2}?/)
+      end
+
+      def to_hash
+        text = body ? body.dup : ""
+        {
+          title: title,
+          body: text,
+          assignee: assignee
+        }
+      end
+    end
   end
 end
--- spec/plugins/issues_spec.rb
+++ spec/plugins/issues_spec.rb
@@ -1,1 +1,24 @@
 require 'spec_helper'
+require 'plugins/issues'
+
+describe Plugins::Issues do
+  describe 'issue parser' do
+    subject { Plugins::Issues::Parser.new text }
+
+    context 'with all options' do
+      let(:text) { "Fix homepage | Homepage graphic is wrong | assign: bob" }
+
+      its(:assignee) { should == 'bob' }
+      its(:title) { should == 'Fix homepage' }
+      its(:body) { should == 'Homepage graphic is wrong' }
+      its(:to_hash) { should == { title: 'Fix homepage', assignee: 'bob', body: 'Homepage graphic is wrong'} }
+    end
+
+    context 'just simple' do
+      let(:text) { "title | body" }
+
+      its(:title) { should == "title" }
+      its(:body) { should == "body" }
+    end
+  end
+end
